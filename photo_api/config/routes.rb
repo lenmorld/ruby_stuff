79172705349f762af149881a34d76f7d86830c90
lenny <lenmorld@gmail.com>
2019-01-01 15:41:35 -0500

    3_rails_api default json output instead of CRUD-like UI

--- photo_api/config/routes.rb
+++ photo_api/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  resources :photos
+  # resources :photos
+  
+  # default /photos to json output
+  resources :photos, defaults: {format: :json}
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
